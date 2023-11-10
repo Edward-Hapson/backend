d91afc1fe2fc89abd29864e2a6d94b23c86d91c2
Edward-Hapson <edwardhapson@gmail.com>
2023-11-10 15:06:26 +0300

    editindex.js

--- index.js
+++ index.js
@@ -1,40 +1,14 @@
 // Import the express module
 const express = require('express');
 const mongoose = require('mongoose')
-const User = require('./models/user')
-const {databaseConnectionWithRetry} = require('./utils/database')
+const USER = require('./models/user')
 
 // Create an Express application
 const app = express();
 
-//connect database
-
- const connectDB = async() =>{
-    mongoose.set('strictQuery', true)
-
-    // if(isconnected){
-    //     console.log('database already connected')
-    //     return;
-    // }
-
-    try {
-        await mongoose.connect('mongodb+srv://hapson:7iCTcZ5Xa7A2eMQ@spems.p0xp0gn.mongodb.net/',{
-            dbName: 'SAPS',
-            // useNewUrlParser: true,
-            // useUnifiedTopology: true
-        })
-
-        // isconnected = true
-
-        console.log('database connected')
-    } catch (error) {
-        console.log(error)
-    }
-}
 // Define a route that sends "Hello, World!" as the response
 app.get('/api', (req, res) => {
-  const user = User.find();
-  res.send(user);
+  res.send('Hello, World!');
 });
 
 // Set the app to listen on port 3000
@@ -42,5 +16,3 @@ const port = 4000;
 app.listen(port, () => {
   console.log(`Server is listening at http://localhost:${port}`);
 });
-
-connectDB();
