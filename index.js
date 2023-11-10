c4718a68fbbdb9c480a7b17e21133b89bf50710c
Edward-Hapson <edwardhapson@gmail.com>
2023-11-10 11:53:29 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+// Import the express module
+const express = require('express');
+
+// Create an Express application
+const app = express();
+
+// Define a route that sends "Hello, World!" as the response
+app.get('/api', (req, res) => {
+  res.send('Hello, World!');
+});
+
+// Set the app to listen on port 3000
+const port = 4000;
+app.listen(port, () => {
+  console.log(`Server is listening at http://localhost:${port}`);
+});
